0390c2854c926ad52fc6fb9b02f7391994681192
Yuliia Kanarovska <yuliia.kanarovska@gmail.com>
2020-09-30 20:15:45 +0000

    fix: resolving merge conflict

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
+import numpy as nps
+import pandas as pd
+
 def format_mixed_types(df):
-"""
-This function is created for formating improper 
-values in columns 'CAMEO_DEUG_2015' and 'CAMEO_INTL_2015'
-"""
+    '''This function is created for formating improper 
+    values in columns CAMEO_DEUG_2015 and CAMEO_INTL_2015'''
     
+    cols = ['CAMEO_DEUG_2015', 'CAMEO_INTL_2015']
+
+    df[cols].replace({'X': np.nan, 'XX': np.nan})
+    df[cols] = df[cols].astype(int)
+
+    return df
